727ee4bb0e17e5add29474c2a3d1075c19399d2d
Yashu Mittal <mittalyashu77@gmail.com>
2019-05-06 20:50:24 +0530

    Add logo, navbar and social icon to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,109 @@
 <template>
-	<div class="footer">
-	</div>
+	<Container>
+		<div class="footer">
+
+			<div class="footer__main">
+				<Logo />
+				<p>© Copyright 2019 CodeCarrot, Inc</p>
+			</div>
+
+			<div class="footer__navbar">
+				<div class="footer__navbar-item">
+					About
+				</div>
+				<div class="footer__navbar-item">
+					Help
+				</div>
+				<div class="footer__navbar-item">
+					Feedback
+				</div>
+				<div class="footer__navbar-item">
+					Privacy
+				</div>
+				<div class="footer__navbar-item">
+					Terms
+				</div>
+			</div>
+
+			<div class="footer__social">
+				<div class="footer__social-item">
+					<g-image src="../../static/github-4x.png" />
+				</div>
+				<div class="footer__social-item">
+					<g-image src="../../static/producthunt-4x.png" />
+				</div>
+				<div class="footer__social-item">
+					<g-image src="../../static/twitter-4x.png" />
+				</div>
+			</div>
+
+		</div>
+	</Container>
 </template>
 
 <script>
-export default {
+import Container from "../layouts/Container"
+import Logo from "./Logo"
 
+export default {
+	name: "Footer",
+	components: {
+		Container,
+		Logo
+	}
 }
 </script>
 
 <style lang='sass'>
+.footer
+	padding: 60px 0
+	display: flex
+	flex-direction: column
 
+	&__main
+		display: flex
+		flex-direction: column
+
+		p
+			color: #B1B2B4
+			margin-top: .625rem
+			font-size: .875rem
+
+	&__navbar
+		display: flex
+		flex-direction: column
+
+		&-item
+			font-size: 1.125rem
+			color: #B1B2B4
+
+	&__social
+		display: flex
+		flex-direction: row
+
+		&-item
+			&:not(:first-child)
+				margin-left: 40px
+
+			img
+				width: 20px
+				height: 20px
+
+@media (max-width: 992px)
+	.footer__navbar
+		margin-top: 1.2rem
+		margin-bottom: 1.2rem
+
+@media (min-width: 992px)
+	.footer
+		padding: 60px 0 100px
+		flex-direction: row
+		justify-content: space-between
+
+		&__navbar
+			flex-direction: row
+
+			&-item
+				&:not(:last-child)
+					margin-right: 1.125rem
 </style>
