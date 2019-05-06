84d5923497f5441684f233f0a76778b050054e9e
Yashu Mittal <mittalyashu77@gmail.com>
2019-05-06 17:37:00 +0530

    Import header and footer

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,25 +1,14 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metaData.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about">About</g-link>
-      </nav>
-    </header>
-    <slot/>
-  </div>
+	<div class="layout">
+		<header theme="dark" />
+		<slot />
+		<footer />
+	</div>
 </template>
 
-<static-query>
-query {
-  metaData {
-    siteName
-  }
-}
-</static-query>
+<script>
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 <style>
 body {
@@ -45,6 +34,5 @@ body {
 }
 
 .nav__link {
-  margin-left: 20px;
 }
 </style>
