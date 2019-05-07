ef9a71605104535eb23d8eda90d4904eb684d035
Yashu Mittal <mittalyashu77@gmail.com>
2019-05-08 00:07:27 +0530

    Global meta tags to default layout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -12,6 +12,109 @@ import Footer from "../components/Footer";
 
 export default {
 	name: "Default",
+	metaInfo: {
+		meta: [
+			{
+				name: 'description',
+				content: 'Thermal is one stop to manage all Git repository at one place by providing you a simple to use graphic user interface with built-in features commits, history, repository settings and more.'
+			},
+			{
+				name: 'keywords',
+				content: 'thermal, git thermal, git gui, free git gui'
+			},
+			{
+				name: 'url',
+				content: 'https://www.gitthermal.com/'
+			},
+			{
+				name: 'copyright',
+				content: '© Copyright. CodeCarrot, Inc.'
+			},
+			{
+				name: 'language',
+				content: 'en'
+			},
+			{
+				name: 'robots',
+				content: 'index, follow'
+			},
+			{
+				name: 'classification',
+				content: 'project'
+			},
+			{
+				name: 'author',
+				content: 'Yashu Mittal, yashu@codecarrot.net'
+			},
+			{
+				name: 'owner',
+				content: 'CodeCarrot, contact@codecarrot.net'
+			},
+
+
+			// Google
+			{
+				itemprop: 'name',
+				content: 'Thermal'
+			},
+			{
+				itemprop: 'description',
+				content: 'Thermal is one stop to manage all Git repository at one place by providing you a simple to use graphic user interface with built-in features commits, history, repository settings and more.'
+			},
+			{
+				itemprop: 'image',
+				content: './meta-image.png'
+			},
+
+			// Facebook
+			{
+				name: 'og:type',
+				content: 'website'
+			},
+			{
+				name: 'og:image',
+				content: './meta-image.png'
+			},
+			{
+				name: 'og:url',
+				content: 'https://www.gitthermal.com/'
+			},
+			{
+				name: 'og:site_name',
+				content: 'Thermal'
+			},
+			{
+				name: 'og:description',
+				content: 'Thermal is one stop to manage all Git repository at one place by providing you a simple to use graphic user interface with built-in features commits, history, repository settings and more.'
+			},
+			{
+				name: "fb:page_id",
+				content: "181159655729057"
+			},
+			{
+				name: "og:email",
+				content: "contact@codecarrot.net"
+			},
+
+			// Twitter
+			{
+				name: 'twitter:description',
+				content: 'Thermal is one stop to manage all Git repository at one place by providing you a simple to use graphic user interface with built-in features commits, history, repository settings and more.'
+			},
+			{
+				name: 'twitter:image',
+				content: './meta-image.png'
+			},
+			{
+				name: 'twitter:url',
+				content: 'https://www.gitthermal.com/'
+			},
+			{
+				name: 'twitter:site',
+				content: '@gitthermal'
+			}
+		]
+	},
 	components: {
 		Header,
 		Footer
