1c35b861160b5b3cf0d14e36d20260aa8a4dd783
Yashu Mittal <mittalyashu77@gmail.com>
2019-05-18 19:06:36 +0530

    Manifest.json file for PWA support

--- src/main.js
+++ src/main.js
@@ -6,5 +6,10 @@ import DefaultLayout from '~/layouts/Default.vue'
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
 	Vue.component('Layout', DefaultLayout)
-	Vue.use(router)
+	Vue.use(router),
+
+	head.link.push({
+		rel: "manifest",
+		href: "/manifest.json"
+	})
 }
