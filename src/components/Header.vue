a6d8d631c8f393e787db4c978ba09e23d4230ce3
Yashu Mittal <mittalyashu77@gmail.com>
2019-05-18 19:22:32 +0530

    Use menu icon as svg component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -48,7 +48,7 @@
 					@click="navbarToggle()"
 					class="header__navbar-menu"
 				>
-					<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 12h18M3 6h18M3 18h18"/></svg>
+					<MenuIcon />
 				</div>
 			</div>
 		</Container>
@@ -59,6 +59,7 @@
 import Container from "../layouts/Container";
 import Logo from "./Logo";
 import Button from "./Button"
+import MenuIcon from "../../static/images/icon/menu.svg"
 import DownloadMixin from "../mixins/download"
 
 export default {
@@ -73,7 +74,8 @@ export default {
   components: {
     Container,
 		Logo,
-		Button
+		Button,
+		MenuIcon
 	},
 	props: {
 		theme: {
