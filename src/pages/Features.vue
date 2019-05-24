9d33d61a669903cac7e314e54365983651b48988
Yashu Mittal <mittalyashu77@gmail.com>
2019-05-24 13:24:39 +0530

    Add hero (heading & description)

--- src/pages/Features.vue
+++ src/pages/Features.vue
@@ -1,21 +1,47 @@
 <template>
 	<Layout>
-		Features...
+		<div class="features__hero">
+			<Container>
+				<div class="features__hero-heading">
+					Features
+				</div>
+				<div class="features__hero-description">
+					One place to manage all of your Git repositories
+				</div>
+			</Container>
+		</div>
 		<call-to-action />
 	</Layout>
 </template>
 
 <script>
+import Container from "../layouts/Container"
 import CallToAction from "../components/hero/CallToAction";
 
 export default {
 	name: "Features",
 	components: {
+		Container,
 		CallToAction
 	}
 }
 </script>
 
 <style lang='sass'>
+.features
+	&__hero
+		background-color: #222831
+		text-align: center
+		padding-top: 3rem
+		padding-bottom: 5rem
 
+		&-heading
+			color: white
+			font-size: 2.5rem
+			line-height: 1.25
+			margin-bottom: .875rem
+
+		&-description
+			color: rgba(#fff, .5)
+			font-size: 1.5rem
 </style>
