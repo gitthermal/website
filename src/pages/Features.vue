b0108c2b1baf9163b020b2e335f1b104d87c520f
Yashu Mittal <mittalyashu77@gmail.com>
2019-05-24 17:12:39 +0530

    Add features list

--- src/pages/Features.vue
+++ src/pages/Features.vue
@@ -1,22 +1,54 @@
 <template>
 	<Layout>
 		<div class="features__hero">
-			<Container>
+			<container>
 				<div class="features__hero-heading">
 					Features
 				</div>
 				<div class="features__hero-description">
 					One place to manage all Git repositories
 				</div>
-			</Container>
+			</container>
 		</div>
+		<div class="features__new">
+			<container :width="830">
+				<new-feature
+					image="./images/commit-graph-feature-4x.png"
+					title="Stats"
+					description="Keep track of the progress of your project by number of commits made on each day."
+				/>
+				<new-feature
+					image="./images/history-feature-4x.png"
+					title="History"
+					description="Browse commit logs in a timeline-style interface with title, author image, and date."
+					:order="1"
+				/>
+				<new-feature
+					image="./images/file-diff-feature-4x.png"
+					title="See the difference"
+					description="Diffs compare versions of your source code side by side, highlighting the parts that are new, edited, or deleted."
+				/>
+				<new-feature
+					image="./images/profile-feature-4x.png"
+					title="Profile"
+					description="Configurate the Git settings by adding your name, email and image."
+					:order="1"
+				/>
+				<new-feature
+					image="./images/commit-feature-4x.png"
+					title="Git commit"
+					description="What took minutes in the command line now takes seconds with Thermal."
+					:mb="0"
+				/>
+			</container>
 		</div>
 		<call-to-action />
 	</Layout>
 </template>
 
 <script>
-import Container from "../layouts/Container"
+import Container from "../layouts/Container";
+import NewFeature from "../components/NewFeature";
 import CallToAction from "../components/hero/CallToAction";
 
 export default {
@@ -32,9 +64,10 @@ export default {
 	},
 	components: {
 		Container,
+		NewFeature,
 		CallToAction
 	}
-}
+};
 </script>
 
 <style lang='sass'>
@@ -54,4 +87,8 @@ export default {
 		&-description
 			color: rgba(#fff, .5)
 			font-size: 1.5rem
+
+	&__new
+		padding-top: 80px
+		padding-bottom: 80px
 </style>
