e3516a924d939b4a5b50a26904cdb521c2d25172
Yashu Mittal <mittalyashu77@gmail.com>
2019-05-24 17:15:42 +0530

    Absolute path for images

--- src/pages/Features.vue
+++ src/pages/Features.vue
@@ -13,29 +13,29 @@
 		<div class="features__new">
 			<container :width="830">
 				<new-feature
-					image="./images/commit-graph-feature-4x.png"
+					image="/images/commit-graph-feature-4x.png"
 					title="Stats"
 					description="Keep track of the progress of your project by number of commits made on each day."
 				/>
 				<new-feature
-					image="./images/history-feature-4x.png"
+					image="/images/history-feature-4x.png"
 					title="History"
 					description="Browse commit logs in a timeline-style interface with title, author image, and date."
 					:order="1"
 				/>
 				<new-feature
-					image="./images/file-diff-feature-4x.png"
+					image="/images/file-diff-feature-4x.png"
 					title="See the difference"
 					description="Diffs compare versions of your source code side by side, highlighting the parts that are new, edited, or deleted."
 				/>
 				<new-feature
-					image="./images/profile-feature-4x.png"
+					image="/images/profile-feature-4x.png"
 					title="Profile"
 					description="Configurate the Git settings by adding your name, email and image."
 					:order="1"
 				/>
 				<new-feature
-					image="./images/commit-feature-4x.png"
+					image="/images/commit-feature-4x.png"
 					title="Git commit"
 					description="What took minutes in the command line now takes seconds with Thermal."
 					:mb="0"
