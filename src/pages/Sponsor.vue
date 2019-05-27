1ccbb83ef1ccc614601043ee47bd7a764916bf58
Yashu Mittal <mittalyashu77@gmail.com>
2019-05-27 16:31:52 +0530

    Remove comments from stripeCheckout method

--- src/pages/Sponsor.vue
+++ src/pages/Sponsor.vue
@@ -56,24 +56,14 @@ export default {
 	methods: {
 		stripeCheckout(plan_id) {
 			var stripe = Stripe("pk_live_DLEhVwauymdEAIPdN07BsLEx00hNZ8kcwt");
-			// When the customer clicks on the button, redirect
-			// them to Checkout.
 			stripe
 				.redirectToCheckout({
 					items: [{ plan: plan_id, quantity: 1 }],
-
-					// Do not rely on the redirect to the successUrl for fulfilling
-					// purchases, customers may not always reach the success_url after
-					// a successful payment.
-					// Instead use one of the strategies described in
-					// https://stripe.com/docs/payments/checkout/fulfillment
 					successUrl: "https://thermal.codecarrot.net/payment/success",
 					cancelUrl: "https://thermal.codecarrot.net/payment/canceled"
 				})
 				.then(function(result) {
 					if (result.error) {
-						// If `redirectToCheckout` fails due to a browser or network
-						// error, display the localized error message to your customer.
 						var displayError = document.getElementById("error-message");
 						displayError.textContent = result.error.message;
 					}
