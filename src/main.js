d24a3d7568a29fcd6336f76252c9b7d3a7b3a8b8
Yashu Mittal <mittalyashu77@gmail.com>
2019-07-04 18:42:22 +0530

    Remove vue-instantsearch codebase

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
-import InstantSearch from 'vue-instantsearch';
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
 	Vue.component('Layout', DefaultLayout)
 	Vue.use(router),
-	Vue.use(InstantSearch);
+
 	// PWA manifest file
 	head.link.push({
 		rel: "manifest",
