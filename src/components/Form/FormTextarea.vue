b5df85d7d95379009b5e93c9ebdfd08e3692d3ba
Yashu Mittal <mittalyashu77@gmail.com>
2019-07-24 00:10:52 +0530

    Create: Bug report tool

---
+++ src/components/Form/FormTextarea.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="form__input">
+		<p class="form__input-label">{{ label }}</p>
+		<textarea type="text" :name="label" @input="$emit('input', $event.target.value)" :placeholder="placeholder" :required="required"></textarea>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "FormTextarea",
+	props: {
+		label: {
+			type: String
+		},
+		placeholder: {
+			type: String
+		},
+		required: {
+			type: Boolean,
+			default: false
+		}
+	}
+}
+</script>
+
+<style lang='sass'>
+
+</style>
---
+++ src/pages/Bug.vue
@@ -0,0 +1,62 @@
+<template>
+	<layout>
+		<container style="padding-top: 3rem; padding-bottom: 3rem;">
+			<h3>Bug report tool</h3>
+			<p>This tool is only for canary tester and it will NOT submit a bug, it's intended use is to help you properly format text that ThermalBot will accept in a reporting.</p>
+			<form-text-input v-model="report.title" placeholder="Title" label="Title" :required="true" />
+			<form-textarea v-model="report.steps" placeholder="Step to reproduce" label="Step to reproduce" :required="true" />
+			<form-text-input v-model="report.expected" placeholder="Expected Result" label="Expected Result" :required="true" />
+			<form-text-input v-model="report.actual" placeholder="Actual Result" label="Actual Result" :required="true" />
+			<form-text-input v-model="report.build" placeholder="Build Number" label="Build Number" :required="true" />
+			<form-text-input v-model="report.system" placeholder="System Settings" label="System Settings" :required="true" />
+			<primary-button
+				text="Submit"
+				:size="1"
+				style="margin-top: 1rem;"
+				@click.native="report.isActive = !report.isActive"
+			/>
+		</container>
+		<div v-show="report.isActive" class="bugreport__result">
+			<div class="bugreport__result-overlay">
+				{{ `!bug | ${report.title} | ${report.steps} | ${report.expected} | ${report.actual} | ${report.build} | ${report.system}` }}
+			</div>
+		</div>
+	</layout>
+</template>
+
+<script>
+import Container from "../layouts/Container";
+import FormTextInput from "../components/Form/FormTextInput";
+import FormTextarea from "../components/Form/FormTextarea";
+import PrimaryButton from "../components/Button/PrimaryButton";
+
+export default {
+	name: "Bug",
+	metaInfo: {
+		title: "Bug report tool",
+	},
+	data() {
+		return {
+			report: {
+				isActive: false,
+				title: "",
+				steps: "",
+				expected: "",
+				actual: "",
+				build: "",
+				system: ""
+			}
+		}
+	},
+	components: {
+		Container,
+		FormTextInput,
+		FormTextarea,
+		PrimaryButton
+	},
+}
+</script>
+
+<style lang='sass'>
+
+</style>
