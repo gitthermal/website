2967c77db234fc519b9ad9ce85319a72cfd86071
Yashu Mittal <mittalyashu77@gmail.com>
2019-10-10 14:20:07 +0530

    Github star component

---
+++ src/components/GithubStar.vue
@@ -0,0 +1,31 @@
+<template>
+	<github-button
+		href="https://github.com/gitthermal/thermal"
+		:data-size="size"
+		:data-show-count="showCount"
+		aria-label="Star gitthermal/thermal on GitHub"
+	>
+		Star
+	</github-button>
+</template>
+
+<script>
+import GithubButton from "vue-github-button"
+
+export default {
+	name: "GithubStar",
+	components: {
+		GithubButton
+	},
+	props: {
+		showCount: {
+			type: Boolean,
+			default: false
+		},
+		size: {
+			type: String,
+			default: "small"
+		}
+	}
+}
+</script>
